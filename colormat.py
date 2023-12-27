e6d5804d935de41fc5c14b0ce00dc9b66828add0
ᏒᎧᏂᎥᏖ ᏗᏰᎴᏬᏝᏝᏗᏂ <74206938+rohitabdullah@users.noreply.github.com>
2023-12-27 14:35:34 -0600

    Update colormat.py

--- colormat.py
+++ colormat.py
@@ -14,13 +14,17 @@ COLORS = {
 }
 
 
+def generate_gradient_color():
+    # Implement your preferred gradient color generation logic here
+    # You can use external libraries or custom algorithms
+    return random.choice(list(COLORS.values()))  # Replace with actual gradient code
+
+
 def cmatrix():
-    # Initialize empty screen with a more efficient data structure
     screen = {}
     for y in range(25):
         screen[y] = {}
 
-    # Define character fall speeds with a focus on slower speeds
     speeds = {
         "slow": 0.3,
         "medium": 0.2,
@@ -28,25 +32,22 @@ def cmatrix():
     }
 
     while True:
-        # Add new characters with adjusted spawn rate
-        for i in range(random.randint(1, 3)):  # Slightly reduced spawn rate
+        for i in range(random.randint(1, 3)):
             column = random.randint(0, 79)
-            color_choice = random.choice(list(COLORS.keys()))
+            color = generate_gradient_color()
             character = chr(random.randint(33, 126))
-            fall_speed = random.choice(["slow", "slow", "slow", "medium", "fast"])  # Favor slower speeds
-            screen[0][column] = f"{COLORS[color_choice]}{character}@{fall_speed}"
+            fall_speed = random.choice(["slow", "slow", "slow", "medium", "fast"])
+            screen[0][column] = f"{color}{character}@{fall_speed}"
 
-        # Shift characters down based on their individual speeds
         for y in range(24, 0, -1):
             for x in range(80):
-                if screen[y-1].get(x) and screen[y-1][x].endswith("@"):
-                    current_speed = float(screen[y-1][x].split("@")[-1])
+                if screen[y - 1].get(x) and screen[y - 1][x].endswith("@"):
+                    current_speed = float(screen[y - 1][x].split("@")[-1])
                     if y + current_speed < 25:
-                        screen[y][x] = screen[y-1][x][:-1] + "@" + str(current_speed)
+                        screen[y][x] = screen[y - 1][x][:-1] + "@" + str(current_speed)
                     else:
-                        del screen[y][x]  # Efficiently remove characters at the bottom
+                        del screen[y][x]
 
-        # Print the current screen with optimized output
         lines = [
             ''.join([char.split("@")[0] for char in screen.get(y, {}).values()])
             for y in range(25)
@@ -55,7 +56,6 @@ def cmatrix():
         print("\033[0m")  # Reset color before new line
         sys.stdout.flush()
 
-        # Increased delay for smoother perceived speed
         time.sleep(0.1)
 
 
